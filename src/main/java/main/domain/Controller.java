c1b57dff28ad137f771335d4418c78eec6b2e759
CaNeRBaDBoY94fT.cEzA <CaNeRBaDBoY94fT.cEzA@cEzAMaIl.cOm>
2016-10-27 14:27:02 +0200

    test

--- src/main/java/main/domain/Controller.java
+++ src/main/java/main/domain/Controller.java
@@ -1,12 +1,5 @@
 package main.domain;
 
-import main.db.dbService;
-import main.db.dbServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,23 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by j on 10/26/16.
  */
 
-@org.springframework.stereotype.Controller
+@RestController
 public class Controller {
 
-//    @Autowired
-//    dbService serv;
-
-//    @RequestMapping(value="/send", method= RequestMethod.GET)
-//    public int send(@RequestBody String payload){
-//        System.out.println(payload);
-//        return 200;
-//    }
-
-    @MessageMapping("/hello")
-    @SendTo("/topic/greetings")
+    @RequestMapping(value="/send", method=RequestMethod.POST)
     public String greeting() throws Exception {
-        Thread.sleep(1000); // simulated delay
-        return "!";
+        return "ABC";
     }
 
 }

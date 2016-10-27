47ff91bb7040f4087e2fe6c3fde216566591dae3
CaNeRBaDBoY94fT.cEzA <CaNeRBaDBoY94fT.cEzA@cEzAMaIl.cOm>
2016-10-27 14:51:47 +0200

    a

--- src/main/java/main/domain/Controller.java
+++ src/main/java/main/domain/Controller.java
@@ -1,5 +1,6 @@
 package main.domain;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
 
     @RequestMapping(value="/send", method=RequestMethod.POST)
-    public String greeting() throws Exception {
+    public String greeting(@RequestBody String body) throws Exception {
+        System.out.println("REQUESTED:" + body);
         return "ABC";
     }
 

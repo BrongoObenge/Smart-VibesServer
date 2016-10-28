924428fa8f081a84cd6a6fff4ca541c00d63a7ef
CaNeRBaDBoY94fT.cEzA <CaNeRBaDBoY94fT.cEzA@cEzAMaIl.cOm>
2016-10-28 02:38:51 +0200

    t

--- src/main/java/main/domain/Controller.java
+++ src/main/java/main/domain/Controller.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URLDecoder;
+
 /**
  * Created by j on 10/26/16.
  */
@@ -13,9 +15,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
 
     @RequestMapping(value="/send", method=RequestMethod.POST)
-    public String greeting(@RequestBody String body) throws Exception {
-        System.out.println("REQUESTED:" + body);
+    public String send(@RequestBody String body) throws Exception {
+        if(body.startsWith("%7B")){
+            System.out.println(URLDecoder.decode(body, "UTF-8"));
+        }
+        System.out.println(body);
         return "ABC";
+    }
+
+    @RequestMapping(value="/retrieve", method=RequestMethod.POST)
+    public String get(@RequestBody String body) throws Exception {
+        System.out.println(body);
+        return "DEF";
     }
 
 }
